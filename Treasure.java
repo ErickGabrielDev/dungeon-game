b989fca85c4c3852fcafe0f25598182f49a6ef28
Erick Gabriel <159559836+ErickGabrielDev@users.noreply.github.com>
2024-07-01 21:44:07 -0300

    Add files via upload

---
+++ Treasure.java
@@ -0,0 +1,27 @@
+class Treasure {
+    private String name;
+    private int value;
+	private boolean collected;
+
+    public Treasure(String name, int value) {
+        this.name = name;
+        this.value = value;
+		this.collected = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+	
+	public boolean isCollected() {
+        return collected;
+    }
+
+    public void collect() {
+        this.collected = true;
+    }	
+}
